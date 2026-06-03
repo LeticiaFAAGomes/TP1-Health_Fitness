8c56a3520cba6c89e41d94a4df066b8e843b25a6
LeticiaFAAGomes <leticia.gomes@al.infnet.edu.br>
2026-06-03 02:30:52 -0300

    feat: add camada model Aluno

--- healthFitness/src/main/java/br/edu/infnet/healthFitness/model/Aluno.java
+++ healthFitness/src/main/java/br/edu/infnet/healthFitness/model/Aluno.java
@@ -1,4 +1,27 @@
 package br.edu.infnet.healthFitness.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+@Data
+@NoArgsConstructor
 public class Aluno {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private Date dataNascimento;
+    private String email;
+    private String telefone;
+
+    public Aluno(String nome, Date dataNascimento, String email, String telefone) {
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.email = email;
+        this.telefone = telefone;
+    }
 }
