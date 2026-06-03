57bd87c0cc2c8a74cb39d75b22cd66333df92f3f
LeticiaFAAGomes <leticia.gomes@al.infnet.edu.br>
2026-06-03 01:13:46 -0300

    feat: add estrutura do projeto

---
+++ healthFitness/src/main/java/br/edu/infnet/healthFitness/HealthFitnessApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.healthFitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthFitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthFitnessApplication.class, args);
+	}
+
+}
---
+++ healthFitness/src/test/java/br/edu/infnet/healthFitness/HealthFitnessApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.healthFitness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthFitnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
